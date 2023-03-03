ea1c3cbb7f461306de56479ae8f40dd560a57ab8
TheBear23 <ntnict@yahoo.com>
2023-03-03 16:42:33 -0600

    corrected latest errors

--- scriptHelper.js
+++ scriptHelper.js
@@ -56,11 +56,11 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "rgb(199, 37, 78)";
       } else {
-        cargoStatus.innerHTML = "Cargo mass low enough for launch";
+        cargoStatus.innerHTML = "Cargo mass too heavy for launch";
       }
   
       if (fuelLevel >= 10000 && cargoMass <= 10000) {
-        faultyItems.style.visibility = "visbile";
+        faultyItems.style.visibility = "visible";
         launchStatus.innerHTML = "Shuttle is Ready for Launch";
         launchStatus.style.color = "rgb(65, 159, 106)";
       }
